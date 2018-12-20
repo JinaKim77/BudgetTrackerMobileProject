7bf5cc1c18ad3ab13a97373ee4495c7b23035365
JinaKim77 <37179130+JinaKim77@users.noreply.github.com>
2018-12-20 17:59:52 +0000

    Update ExpenditureItem.cs

--- MyMobileProject/MyMobileProject/ExpenditureItem.cs
+++ MyMobileProject/MyMobileProject/ExpenditureItem.cs
@@ -1,27 +1,19 @@
-﻿using SQLite;
+using SQLite;
 using System;
 
 namespace MyMobileProjec
 {
     public class ExpenditureItem
     {
+
         [PrimaryKey, AutoIncrement]
-
         public int ID { get; set; }
-
         public string Name { get; set; }
-
-        public double Notes { get; set; }
-
+        public string Notes { get; set; }
         public bool Done { get; set; }
 
-        public string FormattedPrice
-        {
-            get
-            {
-                return "$ " + Math.Round(Notes, 2).ToString("#.00");
-            }
-        }
+
+ 
 
     }
 }
